f43ce4734de49da88321ed24ea2be9aa4df49d53
JeonJimin <cjjunn0827@gmail.com>
2022-04-19 23:14:31 +0900

    220419 1001_a-b

---
+++ Baekjoon/1001_a-b.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftCodeTest
+//
+//  Created by Jeon Jimin on 2022/04/19.
+//
+
+import Foundation
+
+let a: [Int] = readLine()!.split(separator: " ").map { Int(String($0))!}
+print(a[0]-a[1])
+
