683b3229c77dce8f85f13cc876f0938f4b94118c
JeonJimin <cjjunn0827@gmail.com>
2022-04-20 16:47:45 +0900

    220420 2438_star1

---
+++ Baekjoon/2438_star1.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  algorithmsTest
+//
+//  Created by Jeon Jimin on 2022/04/20.
+//
+
+import Foundation
+
+
+let length = Int(readLine()!)!
+
+for i in 0..<length {
+    for j in 0...i{
+        print("*",terminator: "")
+    }
+    print()
+}
+
+
