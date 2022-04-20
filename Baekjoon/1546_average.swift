94360c747a54e5cd4ab701e2b9d4bb5e7656e1da
JeonJimin <cjjunn0827@gmail.com>
2022-04-20 16:45:54 +0900

    220420 1546_average

---
+++ Baekjoon/1546_average.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  algorithmsTest
+//
+//  Created by Jeon Jimin on 2022/04/20.
+//
+
+import Foundation
+
+func newAverageScore (scores: [Double], length: Int, max: Double) -> Double{
+    var newScores: [Double] = []
+    var average: Double = 0
+    for i in 0..<length {
+        newScores.append(scores[i]/max*100)
+    }
+    for i in 0..<length {
+        average += newScores[i]
+    }
+    return average/Double(length)
+}
+
+let length = Int(readLine()!)!
+let score: [Double] = readLine()!.split(separator: " ").map { Double(String($0))!}
+var maxSocre = score.max()
+
+let average = newAverageScore(scores: score, length: length, max: maxSocre!)
+print(average)
+
