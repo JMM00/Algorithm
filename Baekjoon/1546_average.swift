91ac17c84c205a4cd0d7be4b876ce3ef7e371b4c
JeonJimin <cjjunn0827@gmail.com>
2022-04-20 23:00:49 +0900

    220420 1546_average

--- Baekjoon/1546_average.swift
+++ Baekjoon/1546_average.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/*
 func newAverageScore (scores: [Double], length: Int, max: Double) -> Double{
     var newScores: [Double] = []
     var average: Double = 0
@@ -18,11 +19,22 @@ func newAverageScore (scores: [Double], length: Int, max: Double) -> Double{
     }
     return average/Double(length)
 }
-
 let length = Int(readLine()!)!
 let score: [Double] = readLine()!.split(separator: " ").map { Double(String($0))!}
 var maxSocre = score.max()
 
 let average = newAverageScore(scores: score, length: length, max: maxSocre!)
 print(average)
+*/
+
+let length = Int(readLine()!)!
+let score: [Double] = readLine()!.split(separator: " ").map { Double(String($0))!}
+
+var sum: Double = 0
+var maxSocre = score.max()!
+
+for i in 0..<length {
+    sum += score[i]/maxSocre*100
+}
+print (sum/Double(length))
 
