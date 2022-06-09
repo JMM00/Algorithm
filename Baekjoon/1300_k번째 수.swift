9d5facf95d944cbce4303e0afbe96bace14104bc
JeonJimin <cjjunn0827@gmail.com>
2022-06-09 22:34:51 +0900

    220609 1300_k번째 수

---
+++ Baekjoon/1300_k번째 수.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  algorithms_1
+//
+//  Created by Jeon Jimin on 2022/04/26.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+let k = Int(readLine()!)!
+
+var left = 1
+var right = k
+
+var result = 0
+
+while left <= right {
+    var sum = 0
+    let mid = (left + right) / 2
+    
+    for i in 1...n {
+        sum += min(mid/i, n)
+    }
+    
+    if sum < k {
+        left = mid + 1
+    }else{
+        result = mid
+        right = mid - 1
+    }
+}
+print(result)
