8661093f146a9b016fe605014c6132347bdca1b9
Jeon Jimin <41153398+JMM00@users.noreply.github.com>
2022-09-16 04:27:31 +0900

    220916_a+b - 8

---
+++ Baekjoon/11022_a+b8.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  algorithmTest
+//
+//  Created by 전지민 on 2022/09/16.
+//
+
+import Foundation
+
+let count = Int(readLine()!)!
+for i in 1...count {
+    let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+    print("Case #\(i): \(arr[0]) + \(arr[1]) = \(arr[0]+arr[1])")
+}
